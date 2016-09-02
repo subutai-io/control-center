78a4c9cbb0ef076cec7264ba51bc310b92d082c6
okatkov <okatkov@critical-factor.com>
2016-09-02 11:46:29 +0600

    Changed p2p to p2p.exe on windows

--- hub/src/updater/IUpdaterComponent.cpp
+++ hub/src/updater/IUpdaterComponent.cpp
@@ -1,6 +1,10 @@
 #include "updater/IUpdaterComponent.h"
 using namespace update_system;
 
+#ifndef RT_OS_WINDOWS
 const QString IUpdaterComponent::P2P = "p2p";
+#else
+const QString IUpdaterComponent::P2P = "p2p.exe";
+#endif
 const QString IUpdaterComponent::TRAY = "tray";
 const QString IUpdaterComponent::RH = "resource_host";
