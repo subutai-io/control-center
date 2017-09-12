b6ab0ed346ab0344dd0ccc8033e6181454f4477b
okatkov <okatkov@critical-factor.com>
2017-09-12 19:13:42 +0600

    Changed osascript

--- hub/src/SystemCallWrapper.cpp
+++ hub/src/SystemCallWrapper.cpp
@@ -236,19 +236,7 @@ run_ssh_in_terminal_internal<Os2Type<OS_MAC> >(const QString& cmd,
   CApplicationLog::Instance()->LogInfo("Launch command : %s",
                                        str_command.toStdString().c_str());
 
-  if (QSysInfo::productVersion() == "10.12") { //UGLY UGLY HACK!!! ATTENTION
-    CApplicationLog::Instance()->LogInfo("MacOSX version = 10.12");
-    args << QString("Tell application \"Terminal\" do script \"%1\"").arg(str_command);
-    CApplicationLog::Instance()->LogInfo("Do : %s", QString("Tell application \"Terminal\" do script \"%1\"").arg(str_command).toStdString().c_str());
-  } else {
-    CApplicationLog::Instance()->LogInfo("** MacOSX version = %s",
-                                         QSysInfo::productVersion().toStdString().c_str());
-    args << QString("Tell application \"Terminal\"\n"
-                    "  Activate\n"
-                    "  do script \""
-                    "%1\"\n"
-                    " end tell").arg(str_command);
-  }
+  args << QString("Tell application \"Terminal\" to do script \"%1\"\n").arg(str_command);
   return QProcess::startDetached(cmd, args) ? SCWE_SUCCESS : SCWE_SSH_LAUNCH_FAILED;
 }
 /*********************/
