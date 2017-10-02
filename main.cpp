e8ad99518f1e79745e9aad358a9b83b9f858c601
jadilet <adilet.571@gmail.com>
2017-10-02 18:04:23 +0600

    added messages if p2p not present or inoperable with unhealthy p2p debug output

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@
 #include <QSystemSemaphore>
 #include <QMessageBox>
 #include <exception>
+#include <QProcess>
+#include <string>
 #include "TrayControlWindow.h"
 #include "DlgLogin.h"
 #include "TrayWebSocketServer.h"
@@ -131,6 +133,56 @@ main(int argc, char *argv[]) {
       dlg.run_dialog(&sc);
       if (dlg.result() == QDialog::Rejected)
         break;
+
+
+      // start checking p2p is exist
+      CApplicationLog::Instance()->LogInfo("Tray after successfull sign in", "");
+      QProcess p2p;
+
+      p2p.start("which", QStringList() << "p2p");
+
+      if (!p2p.waitForStarted()) {
+          qDebug() << "waiting for p2p started";
+          return false;
+      }
+
+      if (!p2p.waitForFinished()) {
+          qDebug() << "waiting for p2p finished";
+          return false;
+      }
+
+      // not installed p2p
+      if (p2p.exitCode()) {
+          qDebug() << "Not installed p2p, exit code: " << p2p.exitCode();
+          QMessageBox* msg_box = new QMessageBox(QMessageBox::Information, "Error",
+                              "Can't operate without the p2p daemon. Either change the path setting in Settings or install the daemon it is not installed. You can get the [production|dev|stage] daemon from here.",
+                              QMessageBox::Ok);
+          QObject::connect(msg_box, &QMessageBox::finished, msg_box, &QMessageBox::deleteLater);
+          msg_box->exec();
+      } else {
+          p2p.start("p2p", QStringList() << "debug");
+
+          if (!p2p.waitForStarted()) {
+              qDebug() << "waiting for p2p started";
+              return false;
+          }
+
+          if (!p2p.waitForFinished()) {
+              qDebug() << "waiting for p2p finished";
+              return false;
+          }
+
+          // not working p2p daemon
+          if (p2p.exitCode()) {
+              QMessageBox* msg_box = new QMessageBox(QMessageBox::Information, "Error",
+                                  "Your p2p daemon is inoperative. It is either misconfigured or installed incorrectly or both. Please contact us on Slack or file an issue on GitHub.",
+                                  QMessageBox::Ok);
+              QObject::connect(msg_box, &QMessageBox::finished, msg_box, &QMessageBox::deleteLater);
+              msg_box->exec();
+              qDebug() << "Installed but not running p2p, exit code: " << p2p.exitCode();
+          }
+      }
+      // end checking p2p is exist
 
       CTrayServer::Instance()->Init();
       TrayControlWindow tcw;
