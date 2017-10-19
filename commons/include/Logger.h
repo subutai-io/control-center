7fa0eaf302684a0a207607f192a797a09e6b833b
SEFI2 <kadirpili@gmail.com>
2017-10-19 09:49:26 +0600

    checked if log file exists

--- commons/include/Logger.h
+++ commons/include/Logger.h
@@ -31,37 +31,51 @@ public:
         return;
       }
 
-      QFile file;
-      file.setFileName(QString("%1\\logs_%2.txt").arg(CSettingsManager::Instance().logs_storage()).arg(QDate::currentDate().toString("yyyy.MM.dd")));
-      file.open(QIODevice::Append);
-
       QTextStream stdstream(stdout);
-      QTextStream filestream(&file);
       QString timestamp = (QTime::currentTime().toString("HH:mm:ss"));
-
       switch (type) {
         case QtDebugMsg: // 0 level
           stdstream << QString("[%1] [%2:%3] Debug: %4 (in function %5)\n").arg(timestamp).arg(context.file).arg(context.line).arg(msg).arg(context.function);
-          filestream << QString("[%1] [%2:%3] Debug: %4 (in function %5)\n").arg(timestamp).arg(context.file).arg(context.line).arg(msg).arg(context.function);
           break;
         case QtWarningMsg: // 1 level
           stdstream << QString("[%1] [%2:%3] Info: %4 (in function %5)\n").arg(timestamp).arg(context.file).arg(context.line).arg(msg).arg(context.function);
-          filestream << QString("[%1] [%2:%3] Info: %4 (in function %5)\n").arg(timestamp).arg(context.file).arg(context.line).arg(msg).arg(context.function);
           break;
         case QtCriticalMsg: // 2 level
           stdstream << QString("[%1] [%2:%3] Info: %4 (in function %5)\n").arg(timestamp).arg(context.file).arg(context.line).arg(msg).arg(context.function);
-          filestream << QString("[%1] [%2:%3] Info: %4 (in function %5)\n").arg(timestamp).arg(context.file).arg(context.line).arg(msg).arg(context.function);
           break;
         case QtFatalMsg: // 3 level
           stdstream << QString("[%1] [%2:%3] Info: %4 (in function %5)\n").arg(timestamp).arg(context.file).arg(context.line).arg(msg).arg(context.function);
-          filestream << QString("[%1] [%2:%3] Info: %4 (in function %5)\n").arg(timestamp).arg(context.file).arg(context.line).arg(msg).arg(context.function);
           abort();
         case QtInfoMsg: // 4 level
           stdstream << QString("[%1] [%2:%3] Info: %4 (in function %5)\n").arg(timestamp).arg(context.file).arg(context.line).arg(msg).arg(context.function);
+          break;
+     }
+
+
+      QFile file;
+      file.setFileName(QString("%1\\logs_%2.txt").arg(CSettingsManager::Instance().logs_storage()).arg(QDate::currentDate().toString("yyyy.MM.dd")));
+      if (!file.open(QIODevice::Append))
+          return;
+      QTextStream filestream(&file);
+
+      switch (type) {
+        case QtDebugMsg: // 0 level
+          filestream << QString("[%1] [%2:%3] Debug: %4 (in function %5)\n").arg(timestamp).arg(context.file).arg(context.line).arg(msg).arg(context.function);
+          break;
+        case QtWarningMsg: // 1 level
           filestream << QString("[%1] [%2:%3] Info: %4 (in function %5)\n").arg(timestamp).arg(context.file).arg(context.line).arg(msg).arg(context.function);
           break;
-       }
-    }
+        case QtCriticalMsg: // 2 level
+          filestream << QString("[%1] [%2:%3] Info: %4 (in function %5)\n").arg(timestamp).arg(context.file).arg(context.line).arg(msg).arg(context.function);
+          break;
+        case QtFatalMsg: // 3 level
+          filestream << QString("[%1] [%2:%3] Info: %4 (in function %5)\n").arg(timestamp).arg(context.file).arg(context.line).arg(msg).arg(context.function);
+          abort();
+        case QtInfoMsg: // 4 level
+          filestream << QString("[%1] [%2:%3] Info: %4 (in function %5)\n").arg(timestamp).arg(context.file).arg(context.line).arg(msg).arg(context.function);
+          break;
+     }
+  }
 
 };
 
