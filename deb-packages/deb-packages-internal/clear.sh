9e8f56669c29ab37da4c35e9cd364e8aca49e2e3
okatkov <okatkov@optimal-dynamics.com>
2017-10-27 11:41:07 +0600

    Changed debian package

---
+++ deb-packages/deb-packages-internal/clear.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm debian/subutai-tray.*
+rm debian/debhelper-build-stamp
+rm -rf debian/SubutaiTray.tar.gz
+rm -rf debian/icons
+rm -rf debian/SubutaiTray
+rm -rf debian/subutai-tray
+rm -rf debian/libicu52
