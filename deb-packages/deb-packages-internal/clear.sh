29093f94454fc85b03873d8f16801f4ea5f1cbb4
okatkov <okatkov@optimal-dynamics.com>
2017-10-27 12:11:12 +0600

    Changed clear script in deb-packages-internal

--- deb-packages/deb-packages-internal/clear.sh
+++ deb-packages/deb-packages-internal/clear.sh
@@ -7,3 +7,9 @@ rm -rf debian/icons
 rm -rf debian/SubutaiTray
 rm -rf debian/subutai-tray
 rm -rf debian/libicu52
+
+for f in $(ls ../); do
+	if [ "$f" != "deb-packages-internal" ]; then 
+    rm ../$f
+	fi
+done
