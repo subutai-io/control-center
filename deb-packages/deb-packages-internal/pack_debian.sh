47e16a42e844a44b784f07ffc4cf6625bbe9b25b
okatkov <okatkov@optimal-dynamics.com>
2017-10-30 09:14:31 +0600

    Changed papth's in pack_debian script

--- deb-packages/deb-packages-internal/pack_debian.sh
+++ deb-packages/deb-packages-internal/pack_debian.sh
@@ -7,8 +7,8 @@ mkdir -p debian/SubutaiTray/lib
 
 cp ~/SRC/tray/resources/Tray_icon_set-07.png debian/icons
 
-cp subutai_tray_bin/SubutaiTray debian/SubutaiTray/bin
-./dependencies.sh subutai_tray_bin/SubutaiTray debian/SubutaiTray/lib/
+cp ../../subutai_tray_bin/SubutaiTray debian/SubutaiTray/bin
+./dependencies.sh ../../subutai_tray_bin/SubutaiTray debian/SubutaiTray/lib/
 
 cd debian
 tar zcvf SubutaiTray.tar.gz SubutaiTray
