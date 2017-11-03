ecd3a122c88af268b770cf490d9d0fcdf516bda8
jadilet <adilet.571@gmail.com>
2017-11-03 17:12:16 +0600

    Added check locale

--- commons/src/LanguageController.cpp
+++ commons/src/LanguageController.cpp
@@ -14,28 +14,33 @@ LanguageController::~LanguageController() {
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
 LanguageController* LanguageController::Instance() {
-    static LanguageController instance;
-    return &instance;
+  static LanguageController instance;
+  return &instance;
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const QString& LanguageController::LocaleTypeToStr(LOCALE_TYPE lt) {
-   static QString lt_str[] = {tr("English"), tr("Russian"), tr("Brazilian Portuguese")};
-   return lt_str[lt];
+  static QString lt_str[] = {tr("English"), tr("Russian"), tr("Brazilian Portuguese")};
+  return lt_str[lt];
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const QString& LanguageController::LocaleTypeToVal(LOCALE_TYPE lt) {
-    static QString lt_val[] = {"en_US", "ru_RU", "pt_BR"};
-    return lt_val[lt];
+  static QString lt_val[] = {"en_US", "ru_RU", "pt_BR"};
+  return lt_val[lt];
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
 QString LanguageController::CurrentLocale() {
-    QString locale = LanguageController::LocaleTypeToVal(
-                     (LanguageController::LOCALE_TYPE)CSettingsManager::Instance().locale());
-    return locale;
+  QString locale = LanguageController::LocaleTypeToVal(LanguageController::LOCALE_EN);
+
+  if (LanguageController::LAST >= CSettingsManager::Instance().locale()) {
+    locale = LanguageController::LocaleTypeToVal(
+             (LanguageController::LOCALE_TYPE)CSettingsManager::Instance().locale());
+  }
+
+  return locale;
 }
