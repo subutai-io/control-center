11e8d22df94ba016a9536ac59ea6d7e451a4988a
builder <builder@debian.debina-domain>
2017-11-04 01:56:22 -0400

    Changed lib -> Libs. Changed default settings location to ~/.config/subutai/subutai_tray.ini

--- deb-packages/deb-packages-internal/pack_debian.sh
+++ deb-packages/deb-packages-internal/pack_debian.sh
@@ -4,15 +4,16 @@
 mkdir -p debian/icons
 mkdir -p debian/SubutaiTray/bin
 mkdir -p debian/SubutaiTray/lib
+mkdir -p debian/SubutaiTray/Libs
 
 cp ~/SRC/tray/resources/Tray_icon_set-07.png debian/icons
 
 cp ../../subutai_tray_bin/SubutaiTray debian/SubutaiTray/bin
-./dependencies.sh ../../subutai_tray_bin/SubutaiTray debian/SubutaiTray/lib/
+./dependencies.sh ../../subutai_tray_bin/SubutaiTray debian/SubutaiTray/Libs/
 
 cd debian
 tar zcvf SubutaiTray.tar.gz SubutaiTray
 cd ..
 
 #debuild -B -d
-debuild
+debuild 
