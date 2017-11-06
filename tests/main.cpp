e746c25d5d4a22c77b35e06e100521e40502f6ba
jadilet <adilet.571@gmail.com>
2017-11-06 09:52:37 +0600

    Added run tests on console

---
+++ tests/main.cpp
@@ -0,0 +1,12 @@
+#include<QApplication>
+#include "Tester.h"
+
+int
+main(int argc, char *argv[]) {
+    QApplication::setApplicationName("TestingTray");
+    QApplication::setOrganizationName("subut.ai");
+    QApplication app(argc, argv);
+    Tester::Instance()->runAllTest();
+    app.exec();
+    return 0;
+}
