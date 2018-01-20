7bfdd0933c45f7a4f4b8eb0ac5aa4c7c8b146f77
SEFI2 <kadirpili@gmail.com>
2018-01-20 16:50:50 +0600

    enhancement

--- hub/include/DlgEnvironment.h
+++ hub/include/DlgEnvironment.h
@@ -12,25 +12,20 @@ class DlgEnvironment;
 class DlgEnvironment : public QDialog
 {
     Q_OBJECT
-
 public:
-
   explicit DlgEnvironment(QWidget *parent = 0);
-  void addContainer(const CHubContainer*cont);
   void addEnvironment(const CEnvironment *env);
-
   ~DlgEnvironment();
+
+private:
+  Ui::DlgEnvironment *ui;
+  CEnvironment env;
+  std::map<QString,std::pair<QPushButton*, QPushButton*>> dct_cont_btn;
+  void addContainer(const CHubContainer*cont);
   void check_container_status(const CHubContainer *cont, bool &ssh_all, bool &desktop_all);
   void check_environment_status();
   void remote_acces(const CHubContainer &cont, std::pair<QPushButton*, QPushButton*> btns);
   void change_btn(QPushButton *btn, const QString tt_msg, bool enabled);
-
-private:
-    Ui::DlgEnvironment *ui;
-    CEnvironment env;
-    std::map<QString,std::pair<QPushButton*, QPushButton*>>
-      dct_cont_btn; //
-
 
 signals:
     void ssh_to_container_sig(const CEnvironment*, const CHubContainer*, void*);
