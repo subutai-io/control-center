6e7987891cf3a647dec9f6235f65e76f62181ad6
erkin97 <matkaziev.97@gmail.com>
2018-02-12 15:18:21 +0600

    notifications about envs become more readable and organized #610

--- hub/src/TrayControlWindow.cpp
+++ hub/src/TrayControlWindow.cpp
@@ -565,8 +565,10 @@ void TrayControlWindow::environments_updated_sl(int rr) {
   static std::vector<QString> lst_checked_unhealthy_env;
   m_hub_menu->clear();
 
-  std::vector<QString> lst_unhealthy_envs;
-  std::vector<QString> lst_unhealthy_env_statuses;
+  //std::vector<QString> lst_unhealthy_envs;
+  //std::vector<QString> lst_unhealthy_env_statuses;
+
+  std::map<QString, std::vector<QString> > tbl_envs;
 
   if (CHubController::Instance().lst_environments().empty()) {
     QAction* empty_action = new QAction("Empty", this);
@@ -588,9 +590,10 @@ void TrayControlWindow::environments_updated_sl(int rr) {
 
     if (!env->healthy()) {
       if (iter_found == lst_checked_unhealthy_env.end()) {
-        lst_unhealthy_envs.push_back(env_name);
-        lst_unhealthy_env_statuses.push_back(env->status());
+    //    lst_unhealthy_envs.push_back(env_name);
+    //    lst_unhealthy_env_statuses.push_back(env->status());
         lst_checked_unhealthy_env.push_back(env->id());
+        tbl_envs[env->status()].push_back(env_name);
         qCritical(
             "Environment %s, %s is unhealthy. Reason : %s",
             env_name.toStdString().c_str(), env->id().toStdString().c_str(),
@@ -615,7 +618,7 @@ void TrayControlWindow::environments_updated_sl(int rr) {
     });
   }  // for auto env in environments list
 
-  if (lst_unhealthy_envs.empty()) return;
+  /*if (lst_unhealthy_envs.empty()) return;
 
   QString str_unhealthy_envs = "";
   QString str_statuses = "";
@@ -636,7 +639,23 @@ void TrayControlWindow::environments_updated_sl(int rr) {
           .arg(lst_unhealthy_envs.size() > 1 ? "are" : "is")
           .arg(str_statuses);
 
-  CNotificationObserver::Instance()->Info(str_notification, DlgNotification::N_NO_ACTION);
+  CNotificationObserver::Instance()->Info(str_notification, DlgNotification::N_NO_ACTION);*/
+
+  for (std::map<QString, std::vector<QString> >::iterator it = tbl_envs.begin(); it != tbl_envs.end(); it++){
+      if(!it->second.empty()){
+          QString str_env_names = "";
+          for (size_t i=0; i < it->second.size()-1; i++)
+              str_env_names += it->second[i] + ", ";
+          str_env_names += it->second[it->second.size()-1];
+          QString str_notifications =
+              tr("Environment%1 %2 %3 %4")
+                  .arg(it->second.size() > 1 ? "s" : "")
+                  .arg(str_env_names)
+                  .arg(it->second.size() > 1 ? "are" : "is")
+                  .arg(it->first);
+          CNotificationObserver::Instance()->Info(str_notifications, DlgNotification::N_NO_ACTION);
+      }
+  }
 }
 
 ////////////////////////////////////////////////////////////////////////////
