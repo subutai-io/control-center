9ecf8e880f8d020e0c9799c6c650deeb6c340b2d
Erkin <ematkaziev@optimal-dynamics.com>
2018-03-20 12:01:38 +0600

    add path for virtualbox

--- commons/include/OsBranchConsts.h
+++ commons/include/OsBranchConsts.h
@@ -63,6 +63,7 @@ const QString& default_vagrant_path();
 const QString& default_terminal();
 const QString& default_x2goclient_path();
 const QString& default_term_arg();
+const QString& default_oracle_virtualbox_path();
 
 const QString& hub_site();
 const QString& ssh_keygen_cmd_path();
