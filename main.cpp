376c588449ec481d0aa83cbd4f38392d5c50b6e9
erkin97 <matkaziev.97@gmail.com>
2018-05-19 11:02:13 +0600

    Google format on main.cpp

--- main.cpp
+++ main.cpp
@@ -1,32 +1,31 @@
-#include <iostream>
 #include <QApplication>
 #include <QCommandLineParser>
-#include <QFile>
 #include <QDir>
-#include <QSplashScreen>
-#include <QSharedMemory>
-#include <QSystemSemaphore>
+#include <QFile>
 #include <QMessageBox>
-#include <exception>
 #include <QProcess>
+#include <QSharedMemory>
+#include <QSplashScreen>
+#include <QSystemSemaphore>
 #include <QTranslator>
+#include <exception>
+#include <iostream>
 #include <string>
-#include "TrayControlWindow.h"
 #include "DlgLogin.h"
-#include "TrayWebSocketServer.h"
 #include "SettingsManager.h"
+#include "TrayControlWindow.h"
+#include "TrayWebSocketServer.h"
 #include "updater/UpdaterComponentTray.h"
 
-#include "OsBranchConsts.h"
-#include "RhController.h"
-#include "NotificationLogger.h"
-#include "LibsshController.h"
-#include "SystemCallWrapper.h"
-#include "Logger.h"
 #include "LanguageController.h"
+#include "LibsshController.h"
+#include "Logger.h"
+#include "NotificationLogger.h"
+#include "OsBranchConsts.h"
 #include "P2PController.h"
 #include "PeerController.h"
-
+#include "RhController.h"
+#include "SystemCallWrapper.h"
 
 ////////////////////////////////////////////////////////////////////////////
 
@@ -37,11 +36,11 @@
  * \return
  * arguments can be :
  * --v  - uses for getting version of tray application
- * --l  - uses to set log_level. can be 0, 1 and 2. 0 - most detailed. or use "trace", "info" and "error"
+ * --l  - uses to set log_level. can be 0, 1 and 2. 0 - most detailed. or use
+ * "trace", "info" and "error"
  */
 
-int
-main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
   static const char* sem_guid = "6a27ccc9-8b72-4e9f-8d2a-5e25cb389b77";
   static const char* shmem_guid = "6ad2b325-682e-4acf-81e7-3bd368ee07d7";
   QSystemSemaphore sema(sem_guid, 1);
@@ -71,32 +70,38 @@ main(int argc, char *argv[]) {
 
   QTranslator translator;
   QString locale = LanguageController::CurrentLocale();
-  translator.load(QString("SubutaiControlCenter_%1.qm").arg(locale), QApplication::applicationDirPath());
+  translator.load(QString("SubutaiControlCenter_%1.qm").arg(locale),
+                  QApplication::applicationDirPath());
   app.installTranslator(&translator);
 
   qInstallMessageHandler(Logger::LoggerMessageOutput);
 
-  if (is_first && !QApplication::arguments().contains(CCommons::RESTARTED_ARG)) {
-    QMessageBox* msg_box = new QMessageBox(QMessageBox::Information, QObject::tr("Already running"),
-                        QObject::tr("One instance of control center application is already running"),
-                        QMessageBox::Ok);
-    QObject::connect(msg_box, &QMessageBox::finished, msg_box, &QMessageBox::deleteLater);
+  if (is_first &&
+      !QApplication::arguments().contains(CCommons::RESTARTED_ARG)) {
+    QMessageBox* msg_box = new QMessageBox(
+        QMessageBox::Information, QObject::tr("Already running"),
+        QObject::tr(
+            "One instance of control center application is already running"),
+        QMessageBox::Ok);
+    QObject::connect(msg_box, &QMessageBox::finished, msg_box,
+                     &QMessageBox::deleteLater);
     msg_box->exec();
     return 0;
   }
 
   QCommandLineParser cmd_parser;
-  cmd_parser.setApplicationDescription(QObject::tr("This Control Center application should help users to work with hub"));
-  QCommandLineOption log_level_opt("l",
-                                   "Log level can be DEBUG (0), WARNING (1), CRITICAL (2), FATAL (3), INFO (4). Trace is most detailed logs.",
-                                   "log_level",
-                                   "1");
-  QCommandLineOption version_opt("v",
-                                 "Version",
-                                 "Version");
+  cmd_parser.setApplicationDescription(QObject::tr(
+      "This Control Center application should help users to work with hub"));
+  QCommandLineOption log_level_opt(
+      "l",
+      "Log level can be DEBUG (0), WARNING (1), CRITICAL (2), FATAL (3), INFO "
+      "(4). Trace is most detailed logs.",
+      "log_level", "1");
+  QCommandLineOption version_opt("v", "Version", "Version");
 
   cmd_parser.addOption(log_level_opt);
-  cmd_parser.addPositionalArgument("log_level", "Log level to use in this application");
+  cmd_parser.addPositionalArgument("log_level",
+                                   "Log level to use in this application");
   cmd_parser.addOption(version_opt);
   cmd_parser.addHelpOption();
   if (cmd_parser.isSet(version_opt)) {
@@ -108,11 +113,12 @@ main(int argc, char *argv[]) {
   CRhController::Instance()->init();
   qInfo("Tray application %s launched", TRAY_VERSION);
   app.setQuitOnLastWindowClosed(false);
-  qRegisterMetaType<CNotificationObserver::notification_level_t>("CNotificationObserver::notification_level_t");
-  qRegisterMetaType<DlgNotification::NOTIFICATION_ACTION_TYPE>("DlgNotification::NOTIFICATION_ACTION_TYPE");
-  qRegisterMetaType<CEnvironment> ("CEnvironment");
-  qRegisterMetaType<system_call_wrapper_error_t> ("system_call_wrapper_error_t");
-
+  qRegisterMetaType<CNotificationObserver::notification_level_t>(
+      "CNotificationObserver::notification_level_t");
+  qRegisterMetaType<DlgNotification::NOTIFICATION_ACTION_TYPE>(
+      "DlgNotification::NOTIFICATION_ACTION_TYPE");
+  qRegisterMetaType<CEnvironment>("CEnvironment");
+  qRegisterMetaType<system_call_wrapper_error_t>("system_call_wrapper_error_t");
 
   QString tmp[] = {".tmp", "_download"};
   for (int i = 0; i < 2; ++i) {
@@ -120,11 +126,11 @@ main(int argc, char *argv[]) {
     QFile tmp_file(tmp_file_path);
     if (tmp_file.exists()) {
       if (!tmp_file.remove()) {
-        qCritical("Couldn't remove file %s", tmp_file_path.toStdString().c_str());
+        qCritical("Couldn't remove file %s",
+                  tmp_file_path.toStdString().c_str());
       }
     }
   }
-
 
   int result = 0;
   try {
@@ -137,9 +143,7 @@ main(int argc, char *argv[]) {
       sc.show();
 
       dlg.run_dialog(&sc);
-      if (dlg.result() == QDialog::Rejected)
-        break;
-
+      if (dlg.result() == QDialog::Rejected) break;
 
       CTrayServer::Instance()->Init();
       TrayControlWindow::Instance()->Init();
