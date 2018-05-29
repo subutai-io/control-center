2a6a59d5feab501e66c5268eba65d98e37b5c3ca
erkin97 <matkaziev.97@gmail.com>
2018-05-29 15:22:10 +0600

    #1041 update win scripts

--- hub/src/SystemCallWrapper.cpp
+++ hub/src/SystemCallWrapper.cpp
@@ -1166,7 +1166,9 @@ UNUSED_ARG(command);
       str_command += QString("%1 provision 2>> %3_%2 & ").arg(CSettingsManager::Instance().vagrant_path(), command, name);
   }
 
-  str_command += QString("echo finished > %1_finished & exit").arg(*(command.split(" ").begin()));
+  str_command += QString("echo finished > %1_finished & echo Peer finished to %1 with following messages: "
+                         "& type %2_%1 &"
+                         "& set /p DUMMY=Hit ENTER to finish... & exit").arg(*(command.split(" ").begin()), name);
 
   QString cmd;
   QFile cmd_file(CSettingsManager::Instance().terminal_cmd());
