1db03872182a7aecc769880182a8bec776afacbb
erkin97 <matkaziev.97@gmail.com>
2018-06-05 19:45:01 +0600

    declare default kurjun names for boxes

--- commons/include/OsBranchConsts.h
+++ commons/include/OsBranchConsts.h
@@ -58,6 +58,7 @@ const QString& x2go_kurjun_package_name();
 const QString& vagrant_kurjun_package_name();
 const QString& oracle_virtualbox_kurjun_package_name();
 const QString& chrome_kurjun_package_name();
+const QString& subutai_box_kurjun_package_name(const QString &provider);
 const QString& p2p_dht_arg();
 const QString& p2p_rest_url();
 
