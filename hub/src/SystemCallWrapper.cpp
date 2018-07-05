380e17d6b5ebf7a9f688f3734e8ff5a51e101e77
erkin97 <matkaziev.97@gmail.com>
2018-07-05 16:37:27 +0600

    #1096 install dependency seperately

--- hub/src/SystemCallWrapper.cpp
+++ hub/src/SystemCallWrapper.cpp
@@ -1750,7 +1750,6 @@ system_call_wrapper_error_t install_oracle_virtualbox_internal<Os2Type <OS_WIN> 
 }
 template <>
 system_call_wrapper_error_t install_oracle_virtualbox_internal<Os2Type <OS_LINUX> >(const QString &dir, const QString &file_name){
-    QString file_info = dir + "/" + file_name;
     QString pkexec_path;
     system_call_wrapper_error_t scr = CSystemCallWrapper::which("pkexec", pkexec_path);
     if (scr != SCWE_SUCCESS) {
@@ -1791,25 +1790,31 @@ system_call_wrapper_error_t install_oracle_virtualbox_internal<Os2Type <OS_LINUX
       CNotificationObserver::Info(err_msg, DlgNotification::N_SETTINGS);
       return SCWE_CREATE_PROCESS;
     }
-
     QByteArray install_script = QString(
-                                    "#!/bin/bash\n"
-                                    "apt-get install -y dkms build-essential linux-headers-`uname -r`\n"
-                                    "if [ $? -gt 0 ]\n"
-                                    "then\n"
-                                    "apt-get install -y -f\n"
-                                    "apt-get install -y dkms build-essential linux-headers-`uname -r`\n"
-                                    "fi\n"
-                                    "cd %1\n"
-                                    "dpkg -i %2\n"
-                                    "if [ $? -gt 0 ]\n"
-                                    "then\n"
-                                    "dpkg --remove --force-remove-reinstreq %2\n"
-                                    "apt-get install -y -f\n"
-                                    "dpkg -i %2\n"
-                                    "fi\n")
-                                    .arg(dir, file_name)
-                                    .toUtf8();
+      "#!/bin/bash\n"
+      "apt-get install -y dkms\n"
+      "if [ $? -gt 0 ] then\n"
+      "apt-get install -y -f\n"
+      "apt-get install -y dkms\n"
+      "fi\n"
+      "apt-get install -y build-essential\n"
+      "if [ $? -gt 0 ] then\n"
+      "apt-get install -y -f\n"
+      "apt-get install -y build-essential\n"
+      "fi"
+      "apt-get install -y linux-header-`uname -r`\n"
+      "if [ $? -gt 0 ] then\n"
+      "apt-get install -y -f\n"
+      "apt-get install -y linux-header-`uname -r`\n"
+      "fi"
+      "cd %1\n"
+      "dpkg -i %2\n"
+      "if [ $? -gt 0 ]\n"
+      "then\n"
+      "dpkg --remove --force-remove-reinstreq %2\n"
+      "apt-get install -y -f\n"
+      "dpkg -i %2\n"
+      "fi\n").arg(dir, file_name).toUtf8();
 
     if (tmpFile.write(install_script) != install_script.size()) {
       QString err_msg = QObject::tr("Couldn't write install script to temp file")
