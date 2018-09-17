02306c47a65d3267917b54e33d1c434691c81761
jadilet <adilet.571@gmail.com>
2018-09-17 12:53:30 +0600

    added search local vagrant peers #1676

--- hub/src/PeerController.cpp
+++ hub/src/PeerController.cpp
@@ -132,48 +132,13 @@ const QString &CPeerController::provision_step_description(const int &step) {
 void CPeerController::refresh_timer_timeout() { refresh(); }
 
 void CPeerController::search_local() {
-  QDir peers_dir;
-  QStringList stdDirList;
-  QStringList::iterator stdDir;
-
-  switch (VagrantProvider::Instance()->CurrentProvider()) {
-  case VagrantProvider::VIRTUALBOX:
-    // get correct path;
-    stdDirList =
-        QStandardPaths::standardLocations(QStandardPaths::HomeLocation);
-
-    stdDir = stdDirList.begin();
-    if (stdDir == stdDirList.end())
-      peers_dir.setCurrent("/");
-    else
-      peers_dir.setCurrent(*stdDir);
-    break;
-  case VagrantProvider::VMWARE_DESKTOP:
-    peers_dir.setCurrent(CSettingsManager::Instance().vmware_vm_storage());
-    break;
-  default:
-    // get correct path;
-    stdDirList =
-        QStandardPaths::standardLocations(QStandardPaths::HomeLocation);
-
-    QStringList::iterator stdDir = stdDirList.begin();
-    if (stdDir == stdDirList.end())
-      peers_dir.setCurrent("/");
-    else
-      peers_dir.setCurrent(*stdDir);
-    break;
+  // check each peer status
+  for (QString s : this->vagrant_global_status.out) {
+    QFileInfo fi(s);
+    QDir dir(s);
+    get_peer_info(fi, dir);
   }
 
-  if (!peers_dir.absolutePath().contains("Subutai-peers")) {
-    peers_dir.mkdir("Subutai-peers");
-  }
-
-  peers_dir.cd("Subutai-peers");
-
-  // start looking each subfolder
-  for (QFileInfo fi : peers_dir.entryInfoList()) {
-    get_peer_info(fi, peers_dir);
-  }
   if (number_threads == 0) {
     emit got_peer_info(P_STATUS, "update", "peer", "menu");
   }
@@ -300,7 +265,7 @@ void CPeerController::get_peer_info(const QFileInfo &fi, QDir dir) {
   QString peer_name = parse_name(fi.fileName());
   static peer_info_t status_type = P_STATUS;
   if (peer_name == "") return;
-  dir.cd(fi.fileName());
+
   // get status of peer
   GetPeerInfo *thread_for_status = new GetPeerInfo(this);
   number_threads++;
@@ -314,6 +279,9 @@ void CPeerController::get_peer_info(const QFileInfo &fi, QDir dir) {
 }
 
 QString CPeerController::parse_name(const QString &name) {
+  if (!name.contains("subutai-peer"))
+    return name;
+
   QString peer_name;
   QString prefix;
   bool flag = false;
